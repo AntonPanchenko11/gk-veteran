9f84f386459dae8ed341ba1a62c3bab9094f65b4
AntonPanchenko11 <anton.panchenko11@gmail.com>
2025-11-05 11:43:59 -0800

    update content from ContactSection

--- src/presentation/components/ContactsSection/index.tsx
+++ src/presentation/components/ContactsSection/index.tsx
@@ -13,36 +13,29 @@ export function ContactsSection({ className, contentClassName }: ContactsSection
         <div className="mt-8 grid gap-10 md:grid-cols-3">
           <div>
             <p className="body-text">Телефон</p>
-            <p className="mt-2 text-lg font-semibold text-white">8 000 000 00 00</p>
+            <p className="mt-2 text-lg font-semibold text-white">8-989-633-67-99</p>
             <p className="mt-6 body-text">Email</p>
-            <a href="mailto:email@email.ru" className="mt-2 block text-lg font-semibold text-highlight">
-              email@email.ru
+            <a href="mailto:ceo@гк-ветеран.рф" className="mt-2 block text-lg font-semibold text-highlight">
+              ceo@гк-ветеран.рф
             </a>
+            <p className="mt-6 body-text">Юридический адрес</p>
+            <p className="mt-2 text-lg font-semibold text-white">
+              344045, г. Ростов-на-Дону, ул. Миронова, 2Д, кв. 51
+            </p>
           </div>
 
           <div className="space-y-4 body-text">
             <div>
               <p className="font-semibold text-white">ООО «Ветеран. Информационные системы»</p>
-              <p>ИНН / КПП</p>
-              <p>Юридический адрес</p>
-            </div>
-            <div>
-              <p className="font-semibold text-white">ООО «Ветеран. Защита интересов»</p>
-              <p>ИНН / КПП</p>
-              <p>Юридический адрес</p>
+              <p>ИНН / КПП 6163073630 / 616301001</p>
+              <p>ОГРН: 1056163019131</p>
             </div>
           </div>
-
           <div className="space-y-4 body-text">
             <div>
               <p className="font-semibold text-white">ООО «Ветеран. Системный инжиниринг»</p>
-              <p>ИНН / КПП</p>
-              <p>Юридический адрес</p>
-            </div>
-            <div>
-              <p className="font-semibold text-white">ООО «Ветеран. Учебный цетер»</p>
-              <p>ИНН / КПП</p>
-              <p>Юридический адрес</p>
+              <p>ИНН/КПП: 6166133197 / 616601001</p>
+              <p>ОГРН: 1256100017843</p>
             </div>
           </div>
         </div>
